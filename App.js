f5a92a45b6f88180b15cca963997038c83be4539
kenneth34 <kendor34@yahoo.com.sg>
2021-12-18 13:48:09 +0800

    Inital Pt1

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Button, Dimensions } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import BlockRGB from "./BlockRGB";
+import BlockRGB from "./components/BlockRGB";
 //import { TouchableOpacity } from "react-native-gesture-handler";
 
 function HomeScreen({ navigation }) {
  const [colorArray, setColorArray] = useState([
   //Initial value is an array of objects 
-  { red: 255, green: 0, blue: 0, id: "0" },
+   { red: 255, green: 0, blue: 0, id: "0" },
    { red: 0, green: 255, blue: 0, id: "1" },
    { red: 0, green: 0, blue: 255, id: "2" },
  ]);
+ 
  useEffect(() => {
    navigation.setOptions({
      headerRight: () => <Button onPress={addColor} title="Add Color" />
+     
    });
- });
+ 
+  });
  // {item} refers to a single item you pass in
  // eg. {item} = {red:255, green:0, blue: 0, id: "0"}
  function renderItem({ item }) {
@@ -26,8 +29,8 @@ function HomeScreen({ navigation }) {
    // item.green = 0
    // item.blue = 0
    return ( 
-   <TouchableOpacity onPress={() => navigation.navigate("DetailsScreen")}>
-   <BlockRGB red={item.red} green={item.green} blue={item.blue} />
+   <TouchableOpacity onPress={() => navigation.navigate("DetailsScreen", { ...item })}>
+    <BlockRGB red={item.red} green={item.green} blue={item.blue} />
    </TouchableOpacity>
    );
  }
@@ -38,7 +41,7 @@ function HomeScreen({ navigation }) {
    // new list = [...list, 4]
    // which is the same as newList = {1, 2, 3, 4}
   setColorArray([
-    ...colorArray,
+    
     // Add a new object
     {
       // eg. Math.random() give a value from 0 to 1 i.e. 0.8, 0.4
@@ -49,6 +52,7 @@ function HomeScreen({ navigation }) {
       blue: Math.floor(Math.random() * 256),
       id: `${colorArray.length}`,
     },
+    ...colorArray,
   ]);
 }
   function reset() {
@@ -75,8 +79,11 @@ function HomeScreen({ navigation }) {
       <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
     </View>
   );
- 
 }
+
+const windowWidth = Dimensions.get("window").width;
+const numColumns = Math.round(windowWidth / 60);
+//const tileSize = screenWidth / numColumns;
 
 function DetailsScreen({ route }) {
   // Destructure this object so we don't have to type route.params.red etc
@@ -117,13 +124,15 @@ const styles = StyleSheet.create({
    flex: 1,
    backgroundColor: "#fff",
    alignItems: "center",
+   justifyContent: "center",
  },
  list: {
    width: "100%",
  },
  detailText: {
-   fontsize:24,
+   fontSize:30,
    marginBottom:20,
+   justifyContent: "center",
  },
 });
 
